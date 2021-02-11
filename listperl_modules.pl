bb7de430c56f93dc867109b9c5dedcd3f6f98942
Theodore Knab <tknab@chesapeakebay.net>
2021-02-11 07:18:33 -0500

    edit

--- listperl_modules.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-#filename: listperl_modules.pl
-#description: lists the perl modules installed on a system.
-use ExtUtils::Installed;
-my $inst    = ExtUtils::Installed->new();
-my @modules = $inst->modules();
- foreach $module (@modules){
-      print $module . "\n";
-}
